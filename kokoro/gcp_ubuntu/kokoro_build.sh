515b18cba5caa62d279145fdaefab8e1b13b3ee0
PavelJ <pavelgj@gmail.com>
2021-08-19 11:31:00 -0400

    trying to use the cli

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
 # Fail on any error.
-set -e
+set -ex
 
-# Display commands being run.
-# WARNING: please only enable 'set -x' if necessary for debugging, and be very
-#  careful if you handle credentials (e.g. from Keystore) with 'set -x':
-#  statements like "export VAR=$(cat /tmp/keystore/credentials)" will result in
-#  the credentials being printed in build logs.
-#  Additionally, recursive invocation with credentials as command-line
-#  parameters, will print the full command, with credentials, in the build logs.
-# set -x
-
-# Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
-# The final directory name in this path is determined by the scm name specified
-# in the job configuration.
 cd "${KOKORO_ARTIFACTS_DIR}/github/kokoro-codelab-pavelj"
 ./build.sh
 
 curl -sL https://firebase.tools | bash
 
-firebase --help
+echo $GSA_KEY > "${KOKORO_ARTIFACTS_DIR}/gcloud.json"
+export GOOGLE_APPLICATION_CREDENTIALS="$KOKORO_ARTIFACTS_DIR"/gcloud.json
+
+firebase ext:list --project "${PUBLISHER_PROJECT_ID}"
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/firebase-extensions"
 
